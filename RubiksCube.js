dad562030f9d0b48fa37c91fea2508913ca68c02
jayakrishna0511 <dontharaboinajayakrishna@gmail.com>
2025-06-26 16:00:04 +0530

    completed the req

--- RubiksCube.js
+++ RubiksCube.js
@@ -13,6 +13,7 @@ export class RubiksCube {
       R: Array(9).fill('r'),
     };
     this.moveLog = ['-- Reset to Solved State --'];
+    this.scrambleMoves = [];
   }
 
   rotateFaceClockwise(face) {
@@ -24,50 +25,83 @@ export class RubiksCube {
     ];
   }
 
+  rotate(face) {
+    this[`rotate${face}`]();
+    this.moveLog.push(face);
+    this.scrambleMoves.push(face);
+  }
+
   rotateF() {
     this.rotateFaceClockwise('F');
     const { U, R, D, L } = this.faces;
-    const [u6, u7, u8] = [U[6], U[7], U[8]];
-    [U[6], U[7], U[8]] = [L[8], L[5], L[2]];
-    [L[2], L[5], L[8]] = [D[2], D[1], D[0]];
-    [D[0], D[1], D[2]] = [R[6], R[3], R[0]];
-    [R[0], R[3], R[6]] = [u6, u7, u8];
-    this.moveLog.push('F');
+    [U[6], U[7], U[8], L[2], L[5], L[8], D[2], D[1], D[0], R[0], R[3], R[6]] =
+    [L[8], L[5], L[2], D[2], D[1], D[0], R[6], R[3], R[0], U[6], U[7], U[8]];
+  }
+
+  rotateB() {
+    this.rotateFaceClockwise('B');
+    const { U, R, D, L } = this.faces;
+    [U[0], U[1], U[2], R[2], R[5], R[8], D[8], D[7], D[6], L[6], L[3], L[0]] =
+    [R[2], R[5], R[8], D[8], D[7], D[6], L[6], L[3], L[0], U[0], U[1], U[2]];
   }
 
   rotateU() {
     this.rotateFaceClockwise('U');
     const { B, R, F, L } = this.faces;
-    const [b0, b1, b2] = [B[0], B[1], B[2]];
-    [B[0], B[1], B[2]] = [R[0], R[1], R[2]];
-    [R[0], R[1], R[2]] = [F[0], F[1], F[2]];
-    [F[0], F[1], F[2]] = [L[0], L[1], L[2]];
-    [L[0], L[1], L[2]] = [b0, b1, b2];
-    this.moveLog.push('U');
+    [B[0], B[1], B[2], R[0], R[1], R[2], F[0], F[1], F[2], L[0], L[1], L[2]] =
+    [R[0], R[1], R[2], F[0], F[1], F[2], L[0], L[1], L[2], B[0], B[1], B[2]];
+  }
+
+  rotateD() {
+    this.rotateFaceClockwise('D');
+    const { B, R, F, L } = this.faces;
+    [B[6], B[7], B[8], L[6], L[7], L[8], F[6], F[7], F[8], R[6], R[7], R[8]] =
+    [L[6], L[7], L[8], F[6], F[7], F[8], R[6], R[7], R[8], B[6], B[7], B[8]];
+  }
+
+  rotateL() {
+    this.rotateFaceClockwise('L');
+    const { U, F, D, B } = this.faces;
+    [U[0], U[3], U[6], F[0], F[3], F[6], D[0], D[3], D[6], B[8], B[5], B[2]] =
+    [F[0], F[3], F[6], D[0], D[3], D[6], B[8], B[5], B[2], U[0], U[3], U[6]];
   }
 
   rotateR() {
     this.rotateFaceClockwise('R');
     const { U, F, D, B } = this.faces;
-    const [u2, u5, u8] = [U[2], U[5], U[8]];
-    [U[2], U[5], U[8]] = [F[2], F[5], F[8]];
-    [F[2], F[5], F[8]] = [D[2], D[5], D[8]];
-    [D[2], D[5], D[8]] = [B[6], B[3], B[0]];
-    [B[0], B[3], B[6]] = [u2, u5, u8];
-    this.moveLog.push('R');
+    [U[2], U[5], U[8], B[6], B[3], B[0], D[2], D[5], D[8], F[2], F[5], F[8]] =
+    [F[2], F[5], F[8], U[2], U[5], U[8], B[6], B[3], B[0], D[2], D[5], D[8]];
   }
 
   scramble(times = 20) {
-    const moves = ['F', 'U', 'R'];
+    this.scrambleMoves = [];
+    const moves = ['F', 'B', 'U', 'D', 'L', 'R'];
     for (let i = 0; i < times; i++) {
       const move = moves[Math.floor(Math.random() * moves.length)];
-      this[`rotate${move}`]();
+      this.rotate(move);
     }
     this.moveLog.push('-- Scrambled --');
   }
 
-  getFaceColors() {
-    return Object.values(this.faces).flat();
+  solveStepByStep() {
+    const reverseMoves = this.scrambleMoves.slice().reverse().map(move => ({
+      face: move,
+      turns: 3
+    }));
+
+    const solutionSteps = [{ faces: this.cloneFaces(), move: 'Start' }];
+    reverseMoves.forEach(({ face, turns }) => {
+      for (let i = 0; i < turns; i++) {
+        this.rotate(face);
+      }
+      solutionSteps.push({ faces: this.cloneFaces(), move: face + "'" });
+    });
+
+    return solutionSteps;
+  }
+
+  cloneFaces() {
+    return JSON.parse(JSON.stringify(this.faces));
   }
 
   getMoveLog() {
--- script.js
+++ script.js
@@ -18,11 +18,17 @@ const colorMap = {
   y: '#ffeb3b', o: '#ff9800', w: '#ffffff',
 };
 
-function renderCube() {
+let solutionSteps = [];
+let currentStep = 0;
+
+const nextBtn = document.getElementById('nextBtn');
+const prevBtn = document.getElementById('prevBtn');
+
+function renderCubeFromState(faces) {
   container.innerHTML = '';
-  for (const face in cube.faces) {
+  for (const face in faces) {
     const { row, col } = facePositions[face];
-    cube.faces[face].forEach((color, i) => {
+    faces[face].forEach((color, i) => {
       const tile = document.createElement('div');
       tile.className = 'tile';
       tile.style.backgroundColor = colorMap[color];
@@ -31,17 +37,55 @@ function renderCube() {
       container.appendChild(tile);
     });
   }
-  logOutput.textContent = cube.getMoveLog();
 }
 
+function updateNavigationButtons() {
+  prevBtn.style.display = currentStep > 0 ? 'inline-block' : 'none';
+  nextBtn.style.display = currentStep < solutionSteps.length - 1 ? 'inline-block' : 'none';
+}
+
+function updateStep(stepIndex) {
+  if (stepIndex >= 0 && stepIndex < solutionSteps.length) {
+    currentStep = stepIndex;
+    renderCubeFromState(solutionSteps[stepIndex].faces);
+    logOutput.textContent = `Step ${stepIndex}: ${solutionSteps[stepIndex].move}`;
+    updateNavigationButtons();
+  }
+}
+
+// Event Listeners
 document.getElementById('shuffleBtn').addEventListener('click', () => {
-  cube.scramble(20);
-  renderCube();
-});
-
-document.getElementById('mixBtn').addEventListener('click', () => {
   cube.reset();
-  renderCube();
+  cube.scramble(20);
+  renderCubeFromState(cube.faces);
+  logOutput.textContent = cube.getMoveLog();
 });
 
-renderCube();
+document.getElementById('solveBtn').addEventListener('click', () => {
+  solutionSteps = cube.solveStepByStep();
+  updateStep(0);
+});
+
+nextBtn.addEventListener('click', () => {
+  updateStep(currentStep + 1);
+});
+
+prevBtn.addEventListener('click', () => {
+  updateStep(currentStep - 1);
+});
+
+document.getElementById('resetBtn').addEventListener('click', () => {
+  cube.reset();
+  solutionSteps = [];
+  currentStep = 0;
+  renderCubeFromState(cube.faces);
+  logOutput.textContent = '-- Reset to Solved State --';
+
+  // Hide buttons after reset
+  nextBtn.style.display = 'none';
+  prevBtn.style.display = 'none';
+});
+
+// Initial render
+renderCubeFromState(cube.faces);
+logOutput.textContent = '-- Ready --';
